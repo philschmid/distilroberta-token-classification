d78d5628355d3839721d17cb20fde614fa831d4f
philschmid <schmidphilipp1995@gmail.com>
2021-05-19 17:09:12 +0200

    changed compute metric

--- src/training/hyperparameter_search_train.py
+++ src/training/hyperparameter_search_train.py
@@ -123,7 +123,28 @@ def main(args):
 
     # metric
     metric = load_metric("seqeval")
-    compute_metrics = prepare_compute_metrics(metric, label_list)
+
+    def compute_metrics(p):
+        predictions, labels = p
+        predictions = np.argmax(predictions, axis=2)
+
+        # Remove ignored index (special tokens)
+        true_predictions = [
+            [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+        true_labels = [
+            [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+
+        results = metric.compute(predictions=true_predictions, references=true_labels)
+        return {
+            "precision": results["overall_precision"],
+            "recall": results["overall_recall"],
+            "f1": results["overall_f1"],
+            "accuracy": results["overall_accuracy"],
+        }
 
     # load model
     model_init = prepare_model_init(args.model_name_or_path, num_labels)
